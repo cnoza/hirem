55884e344927dfc85ebba39cb22b38cd7cecda0f
nguyenmaihan <christophe.nozaradan@gmail.com>
2022-01-07 17:06:51 +0100

    simulations

--- Examples/render_rmd_baseline.R
+++ Examples/render_rmd_baseline.R
@@ -1,6 +1,6 @@
 # Launch Rmd file for different seeds
 
-seed <- 5
+seed <- 1
 rmarkdown::render("./Examples/Baseline with seed param.Rmd",
                   output_file=paste0("./Examples/baseline/baseline_seed_",seed,".html"),
                   output_format = "all")

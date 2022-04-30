3b88be30c81939b486fee5f8ca4c74a6c6bc7d4b
NOZARADAN Christophe <christophe.nozaradan@soprasteria.com>
2022-04-30 15:24:13 +0200

    adding par_dep function for PDP

--- R/partial_dependence_plots.R
+++ R/partial_dependence_plots.R
@@ -1,3 +1,4 @@
+#' @export
 par_dep <- function(object, data, grid) {
   # Initialize a vector to save the effect
   pd_effect <- rep(0, nrow(grid))
@@ -126,71 +127,4 @@ par_dep <- function(object, data, grid) {
   }
   return(pd_effect)
 }
-#'
 
-df.train <- reserving_data %>% dplyr::filter(calendar.year <= 9, open == 1)
-#df.train_sample <- df.train[sample(seq_len(nrow(df.train)), size = 10000), ]
-
-# Partial dependence plots for "type" covariate
-
-grid_type_init <- data.frame('type' = as.factor(levels(df.train$type)))
-
-# Response variable: settlement
-
-grid_type_settle <- grid_type_init %>%
-  dplyr::mutate(glm = model_glm$layers$settlement %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(gbm = model_gbm$layers$settlement %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(xgb = model_xgb$layers$settlement %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(dnn = model_dnn$layers$settlement %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(cann = model_cann$layers$settlement %>% par_dep(data = df.train, grid = grid_type_init))
-
-# PDPlot
-
-pd_type_settle <- grid_type_settle %>% reshape2::melt(id.vars = 'type',
-                              value.name = 'pd_settlement',
-                              variable.name = 'method') %>%
-  ggplot(aes(x = type, y = pd_settlement)) + theme_bw() +
-  geom_line(aes(group = method, colour = method))
-
-pd_type_settle
-
-# Response variable: payment
-
-grid_type_payment <- grid_type_init %>%
-  dplyr::mutate(glm = model_glm$layers$payment %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(gbm = model_gbm$layers$payment %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(xgb = model_xgb$layers$payment %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(dnn = model_dnn$layers$payment %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(cann = model_cann$layers$payment %>% par_dep(data = df.train, grid = grid_type_init))
-
-# PDPlot
-
-pd_type_payment <- grid_type_payment %>% reshape2::melt(id.vars = 'type',
-                             value.name = 'pd_payment',
-                             variable.name = 'method') %>%
-  ggplot(aes(x = type, y = pd_payment)) + theme_bw() +
-  geom_line(aes(group = method, colour = method))
-
-pd_type_payment
-
-# Response variable: size
-
-grid_type_size <- grid_type_init %>%
-  dplyr::mutate(glm = model_glm$layers$size %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(gbm = model_gbm$layers$size %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(xgb = model_xgb$layers$size %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(dnn = model_dnn$layers$size %>% par_dep(data = df.train, grid = grid_type_init)) %>%
-  dplyr::mutate(cann = model_cann$layers$size %>% par_dep(data = df.train, grid = grid_type_init))
-
-# PDPlot
-
-pd_type_size <- grid_type_size %>% reshape2::melt(id.vars = 'type',
-                             value.name = 'pd_size',
-                             variable.name = 'method') %>%
-  ggplot(aes(x = type, y = pd_size)) + theme_bw() +
-  geom_line(aes(group = method, colour = method))
-
-pd_type_size
-
-library(gridExtra)
-grid.arrange(pd_type_settle,pd_type_payment,pd_type_size,ncol=3)

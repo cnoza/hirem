e1531f0c114a9951d3e0b588cfe4347f4035fafb
Christophe Nozaradan <31512667+cnoza@users.noreply.github.com>
2022-11-24 08:33:34 +0100

    latest changes

--- Examples/render_rmd_wang-wuthrich.R
+++ Examples/render_rmd_wang-wuthrich.R
@@ -1,6 +1,6 @@
 # Launch Rmd file for different seeds
 
-for(seed in 1:1) {
+for(seed in 3:3) {
   rmarkdown::render("./Examples/Wang-Wuthrich with seed param.Rmd",
                     output_file=paste0("./Examples/wang-wuthrich/html-no-cv/wang-wuthrich_seed_",seed,".html"),
                     output_format = "all")

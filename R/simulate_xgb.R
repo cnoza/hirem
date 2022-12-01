2613a82c8008ec217b0b189d9550a69896f57eac
Christophe Nozaradan <31512667+cnoza@users.noreply.github.com>
2022-12-01 13:35:13 +0100

    Update to avoid logical type issue with balance.correction for size.recov layer

--- R/simulate_xgb.R
+++ R/simulate_xgb.R
@@ -9,27 +9,36 @@ simulate.layer_xgb <- function(obj, data, balance.correction, balance.var) {
 
   data_baked <- bake(obj$data_recipe, new_data = data[select,])
   newdata <- select(data_baked,-as.name(label)) %>% as.matrix()
-  newdata.xgb <- xgb.DMatrix(data = newdata, info = list('label' = as.matrix(data[select,label])))
-  response <- predict(obj$fit, ntreelimit = obj$fit$niter, newdata = newdata.xgb, type = 'response')
-
-  if(balance.correction) {
-    response <- response * obj$balance.correction[(data[select,])[[balance.var]]]
-  }
-
-  if(obj$method_options$objective == 'binary:logistic') {
-    simulation <- runif(length(response)) < response
-  } else if(obj$method_options$objective == 'reg:squarederror') {
-    simulation <- rnorm(length(response), mean = response, sd = obj$sigma)
-  } else if(obj$method_options$objective == 'reg:gamma') {
-    simulation <- rgamma(length(response), scale = response / obj$shape, shape = obj$shape)
-  }
-
-  if(!is.null(obj$transformation)) {
-    simulation <- obj$transformation$inverse_transform(simulation)
-  }
 
   ret <- rep(0, nrow(data))
-  ret[select] <- simulation
+
+  if(!is.logical(newdata)) {
+
+    newdata.xgb <- xgb.DMatrix(data = newdata, info = list('label' = as.matrix(data[select,label])))
+    response <- predict(obj$fit, ntreelimit = obj$fit$niter, newdata = newdata.xgb, type = 'response')
+
+    if(balance.correction) {
+      response <- response * obj$balance.correction[(data[select,])[[balance.var]]]
+    }
+
+    if(obj$method_options$objective == 'binary:logistic') {
+      simulation <- runif(length(response)) < response
+    } else if(obj$method_options$objective == 'reg:squarederror') {
+      simulation <- rnorm(length(response), mean = response, sd = obj$sigma)
+    } else if(obj$method_options$objective == 'reg:gamma') {
+      simulation <- rgamma(length(response), scale = response / obj$shape, shape = obj$shape)
+    }
+
+    if(!is.null(obj$transformation)) {
+      simulation <- obj$transformation$inverse_transform(simulation)
+    }
+
+    ret[select] <- simulation
+
+  }
+
+  #ret <- rep(0, nrow(data))
+  #ret[select] <- simulation
 
   return(ret)
 }

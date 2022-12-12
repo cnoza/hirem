1b316b19badecc1b9d503e901b07522045583055
Christophe Nozaradan <31512667+cnoza@users.noreply.github.com>
2022-12-12 10:54:38 +0100

    latest updates

--- Examples/mixed/render_rmd_mixed.R
+++ Examples/mixed/render_rmd_mixed.R
@@ -1,7 +1,7 @@
 # Launch Rmd file for different seeds
 
-for(seed in 1:1) {
-  rmarkdown::render("./Examples/Mixed claims with seed param.Rmd",
+for(seed in 2:2) {
+  rmarkdown::render("./Examples/mixed/Mixed claims with seed param.Rmd",
                     output_file=paste0("./Examples/mixed/html-no-cv/mixed_claims_seed_",seed,".html"),
                     output_format = "all")
   rm(list = setdiff(ls(), seed))

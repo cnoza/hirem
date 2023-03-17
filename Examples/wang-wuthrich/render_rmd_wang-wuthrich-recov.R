863b19fa0c53e0503c0a856cfe32c0bd30566ad1
Christophe Nozaradan <31512667+cnoza@users.noreply.github.com>
2023-03-17 18:07:06 +0100

    .

--- Examples/wang-wuthrich/render_rmd_wang-wuthrich-recov.R
+++ Examples/wang-wuthrich/render_rmd_wang-wuthrich-recov.R
@@ -1,8 +1,8 @@
 # Launch Rmd file for different seeds
 
-for(seed in 1:5) {
-  rmarkdown::render("./Examples/wang-wuthrich/Wang-Wuthrich with seed param (with recoveries 8).Rmd",
-                    output_file=paste0("./Examples/wang-wuthrich/html-no-cv/wang-wuthrich-recov8_seed_",seed,".html"),
+for(seed in 1:3) {
+  rmarkdown::render("./Examples/wang-wuthrich/Wang-Wuthrich with seed param (with recoveries 10).Rmd",
+                    output_file=paste0("./Examples/wang-wuthrich/html-no-cv/run1/wang-wuthrich-recov10_seed_",seed,".html"),
                     output_format = "all")
   rm(list = setdiff(ls(), seed))
 }
